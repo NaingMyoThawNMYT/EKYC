c99af3f2e1679a3b04c7b11833c3d66dc76a314e
NaingMyoThawNMYT <naingmyothaw.nmyt@gmial>
2020-02-04 17:47:56 +0630

    - Change RetrofitHelper to package access modifier.

--- app2/src/main/java/com/qbayar/app2/retrofitHelper/RetrofitHelper.java
+++ app2/src/main/java/com/qbayar/app2/retrofitHelper/RetrofitHelper.java
@@ -3,8 +3,8 @@ package com.qbayar.app2.retrofitHelper;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-public class RetrofitHelper {
-    public static Retrofit getRetrofit(String baseUrl) {
+class RetrofitHelper {
+    static Retrofit getRetrofit(String baseUrl) {
         return new Retrofit
                 .Builder()
                 .baseUrl(baseUrl)
