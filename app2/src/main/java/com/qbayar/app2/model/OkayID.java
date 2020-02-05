076cdc1431b61133e38473a5f52828e017631f89
NaingMyoThawNMYT <naingmyothaw.nmyt@gmial>
2020-02-05 09:55:05 +0630

    - Removed unused file. - Refactored OkayID.

--- app2/src/main/java/com/qbayar/app2/model/OkayID.java
+++ app2/src/main/java/com/qbayar/app2/model/OkayID.java
@@ -1,28 +1,59 @@
 package com.qbayar.app2.model;
 
-import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
-public class OkayID implements Serializable {
+public class OkayID {
+    @SerializedName("base64ImageString")
+    @Expose
     private String base64ImageString;
+    @SerializedName("backImage")
+    @Expose
     private String backImage;
+    @SerializedName("apiKey")
+    @Expose
     private String apiKey;
+    @SerializedName("imageFormat")
+    @Expose
     private String imageFormat;
+    @SerializedName("imageEnabled")
+    @Expose
     private boolean imageEnabled;
+
+    public String getBase64ImageString() {
+        return base64ImageString;
+    }
 
     public void setBase64ImageString(String base64ImageString) {
         this.base64ImageString = base64ImageString;
+    }
+
+    public String getBackImage() {
+        return backImage;
     }
 
     public void setBackImage(String backImage) {
         this.backImage = backImage;
     }
 
+    public String getApiKey() {
+        return apiKey;
+    }
+
     public void setApiKey(String apiKey) {
         this.apiKey = apiKey;
     }
 
+    public String getImageFormat() {
+        return imageFormat;
+    }
+
     public void setImageFormat(String imageFormat) {
         this.imageFormat = imageFormat;
+    }
+
+    public boolean isImageEnabled() {
+        return imageEnabled;
     }
 
     public void setImageEnabled(boolean imageEnabled) {
